afcc1cac284125c18bc773412a1409e39be69c96
w0lf3n <w0lf3n@coole-files.de>
2023-09-12 14:14:07 +0200

    move path to solver class

--- src/pathfinder/solvers/Solver.ts
+++ src/pathfinder/solvers/Solver.ts
@@ -1,14 +1,27 @@
+import type Cell from "../classes/Cell";
+import CellStore from "../classes/CellStore";
+import type Grid from "../classes/Grid";
 import type IPosition from "../classes/IPosition";
 import Worker from "../classes/Worker"
 
 class Solver extends Worker {
 
-    // override set_start_position(position: IPosition) {
-    //     const { x, y } = position
+    protected path: CellStore
 
-    //     super.set_start_position(position)
+    constructor(grid: Grid) {
+        super(grid)
+        this.path = new CellStore()
+    }
 
-    // }
+    override set_start_position(position: IPosition): Cell | undefined {
+        // TODO select correct region, etc..
+        return super.set_start_position(position)
+    }
+
+    draw_path(): void {
+
+    }
+
 }
 
 export default Solver
