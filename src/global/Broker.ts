a6559ad02b99fae6d807fbc1cb4f0de508b679d5
w0lf3n <w0lf3n@coole-files.de>
2023-09-21 09:39:09 +0200

    improve broker via TDD

--- src/global/Broker.ts
+++ src/global/Broker.ts
@@ -1,19 +1,26 @@
-const subscriptions: Record<string, Array<(value: string) => void>> = {}
+type Callback = (topic: string) => void
+const subscriptions: Map<string, Callback[]> = new Map<string, Callback[]>()
 
-export const publish = (id: string, value?: string): void => {
-    if (id in subscriptions) {
-        subscriptions[id].forEach((subscription) => {
-            subscription(value ?? "")
-        })
+export const get_all_subscriptions = (): string[] => [...subscriptions.keys()]
+
+export const publish = (id: string, message?: string): void => {
+    // @see https://stackoverflow.com/questions/70723319/object-is-possibly-undefined-using-es6-map-get-right-after-map-set
+    // @ts-expect-error: Typescript doesn't keep track of the .set .get references
+    subscriptions.get(id).forEach((callback) => { callback(message ?? "") })
+}
+
+export const reset = (): void => {
+    for (const key of subscriptions.keys()) {
+        subscriptions.delete(key)
     }
 }
 
-export const subscribe = (id: string, callback: (value: string) => void): void => {
-    if (id in subscriptions) {
-	    subscriptions[id].push(callback);
-  	} else {
-    	Object.defineProperty(subscriptions, id, {
-  			value: [callback]
-		})
+export const subscribe = (topic: string, callback: Callback): void => {
+    if (subscriptions.has(topic)) {
+        // @see https://stackoverflow.com/questions/70723319/object-is-possibly-undefined-using-es6-map-get-right-after-map-set
+        // @ts-expect-error: Typescript doesn't keep track of the .set .get references
+        subscriptions.get(topic).push(callback)
+    } else {
+        subscriptions.set(topic, [callback])
   	}
 }
---
+++ tests/global/broker.test.ts
@@ -0,0 +1,75 @@
+import {
+    beforeEach,
+    describe,
+    expect,
+    it
+} from 'vitest'
+import {
+    get_all_subscriptions,
+    publish,
+    reset as reset_broker,
+    subscribe
+} from '../../src/global/Broker'
+
+describe("test Cell class", () => {
+
+    beforeEach(() => {
+        reset_broker()
+    })
+
+    it("subscribe", () => {
+        const topics = ["TEST_TOPIC_1", "TEST_TOPIC_2", "TEST_TOPIC_3"]
+
+        const add_subscription = (topic: string): void => { subscribe(topic, (): void => { /* void */ }) }
+        topics.forEach((topic) => { add_subscription(topic) })
+
+        expect(get_all_subscriptions()).toEqual(topics)
+
+        subscribe("NEW_TOPC", (): void => { /* void */ })
+        expect(get_all_subscriptions()).not.toEqual(topics)
+    })
+
+    it("published message will be forwarded to all subscribers", () => {
+        const topic = "test_topic"
+
+        let result1: string = ""
+        const update1 = (message: string): void => { result1 = message }
+        console.log("subscribe from test")
+        subscribe(topic, update1)
+
+        let result2: string = ""
+        const update2 = (message: string): void => { result2 = message }
+        console.log("subscribe from test")
+        subscribe(topic, update2)
+
+        expect(get_all_subscriptions()).toEqual([topic])
+
+        const message = "test_message"
+        publish(topic, message)
+
+        expect(result1).to.be.equal(message)
+        expect(result2).to.be.equal(message)
+    })
+
+    it("publish without message", () => {
+        const topic = "test_topic"
+
+        let result1: string = "init"
+        const update1 = (message: string): void => { result1 = message }
+        console.log("subscribe from test")
+        subscribe(topic, update1)
+
+        let result2: string = "init"
+        const update2 = (message: string): void => { result2 = message }
+        console.log("subscribe from test")
+        subscribe(topic, update2)
+
+        expect(get_all_subscriptions()).toEqual([topic])
+
+        publish(topic)
+
+        expect(result1).to.be.equal("")
+        expect(result2).to.be.equal("")
+    })
+
+})
